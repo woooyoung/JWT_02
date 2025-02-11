f14d78e13ee8959559d9ffe58f72f2a65b14da6e
woooyoung <axdsw121@gmail.com>
2025-02-11 15:52:20 +0900

    accessToken 생성

--- src/test/java/com/koreait/jwt_02/Jwt02ApplicationTests.java
+++ src/test/java/com/koreait/jwt_02/Jwt02ApplicationTests.java
@@ -1,13 +1,17 @@
 package com.koreait.jwt_02;
 
+import com.koreait.jwt_02.base.jwt.JwtProvider;
 import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,6 +20,8 @@ class Jwt02ApplicationTests {
 
     @Value("${custom.jwt.secretKey}")
     private String secretKeyPlain;
+    @Autowired
+    private JwtProvider jwtProvider;
 
 
     @Test
@@ -36,4 +42,35 @@ class Jwt02ApplicationTests {
         assertThat(secretKey).isNotNull();
     }
 
+    @Test
+    @DisplayName("JwtProvider 객체로 SecretKey 객체 생성")
+    void t3() {
+        SecretKey secretKey = jwtProvider.getSecretKey();
+
+        assertThat(secretKey).isNotNull();
+    }
+
+    @Test
+    @DisplayName("SecretKey 객체는 단 한번만 생성 되어야 함")
+    void t4() {
+        SecretKey secretKey1 = jwtProvider.getSecretKey();
+        SecretKey secretKey2 = jwtProvider.getSecretKey();
+
+        assertThat(secretKey1 == secretKey2).isTrue();
+    }
+
+    @Test
+    @DisplayName("accessToken 얻기")
+    void t5() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", 1L);
+        claims.put("username", "admin");
+
+        // 현재시각으로부터 5시간의 유효기간을 가지는 토큰 생성
+        String accessToken = jwtProvider.genToken(claims, 60 * 60 * 5);
+
+        System.out.println("accessToken: " + accessToken);
+
+        assertThat(accessToken).isNotNull();
+    }
 }
