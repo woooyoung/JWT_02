56828681228c47c21e4c1510a7d7ae4cb2eae7cb
woooyoung <axdsw121@gmail.com>
2025-02-11 15:19:20 +0900

    프로젝트 세팅

---
+++ src/main/java/com/koreait/jwt_02/Jwt02Application.java
@@ -0,0 +1,13 @@
+package com.koreait.jwt_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwt02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/jwt_02/Jwt02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.jwt_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
